73b42ca89bc1f176a62a008e4d56b8f3e7e401f8
Prince <princeoboesam@gmail.com>
2026-08-03 05:25:59 +0000

    Fix vector_store search

--- app/rag/retrieval.py
+++ app/rag/retrieval.py
@@ -5,11 +5,15 @@ from langchain_community.retrievers import BM25Retriever
 
 from .chunker import chunks
 from .embedding import get_embedding_model
+from app.config import BASE_DIR
+
+
+CHROMA_DIR = BASE_DIR / "data" / "chroma_db"
 
 def get_retriever(k:int = 5):
-    vector_store = Chroma.from_documents(
-        documents=chunks,
-        embedding=get_embedding_model()
+    vector_store = Chroma(
+        persist_directory=CHROMA_DIR,
+        embedding_function=get_embedding_model()
     )
     vector_retriever = vector_store.as_retriever(search_type="mmr", search_kwargs={"k": k})
 
@@ -24,5 +28,9 @@ def get_retriever(k:int = 5):
         ],
         weights=[0.7, 0.3]
     )
+    docs = retriever.invoke('Do you accept apex?')
+    print(len(docs))
 
     return retriever
+
+get_retriever()
